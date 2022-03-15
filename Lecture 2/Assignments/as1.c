cddbc34630fc6411060e7ce2e8c5c076a3ea8b38
JudelleGaza <100169794+JudelleGaza@users.noreply.github.com>
2022-03-15 14:13:26 +0800

    Add files via upload

---
+++ Lecture 2/Assignments/as1.c
@@ -0,0 +1,17 @@
+//Judelle Gaza
+
+#include <stdio.h>
+int main(void)
+{
+    /*Declaring variable types*/
+    int num1, num2;
+
+    /*Asking for user input and scanning for the 2-digit input*/
+    printf("Please enter a 2-digit number: ");
+    scanf("%1d%1d",&num1,&num2);
+
+    /*Reversing the order of the numbers*/
+    printf("Reverse: %1d%1d", num2, num1);
+    
+    return 0;
+}
